f12e0181a483808f20a40724ca0f6200c7f036ff
sudheerkumarpothineni <sudheerchowdary543@gmail.com>
2025-10-24 16:26:27 +0530

    Javascript Day 1 Session

---
+++ one.js
@@ -0,0 +1,10 @@
+console.log("This is javascript day 1 session")
+
+let name = "sudheer";
+let age = 35;
+
+console.log("I am "+name+' and, i am '+age+' Years old');
+
+// This is single line comment
+
+/* This is multiple line comment */
