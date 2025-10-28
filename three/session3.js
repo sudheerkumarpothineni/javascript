2f9a9c9cf5298d0b30d1e8120fda051f131f679e
sudheerkumarpothineni <sudheerchowdary543@gmail.com>
2025-10-28 21:24:31 +0530

    Javascript Day 3

---
+++ three/session3.js
@@ -0,0 +1,29 @@
+/*
+    Loops
+    -----
+    It will iterate until given condition is true
+
+    for
+    foreach
+    while
+    do...while
+
+    for...of - used for arrays
+    eg
+    --
+    let fruits = ["apple", "guva", "orange"];
+    for(let fruit of fruits){
+        console.log(fruit);
+    }
+    
+    for...in used for objects
+    eg
+    --
+    let person = {name: "Sudheer", age: 35, city: "Hyderabad"};
+    for(let key in person){
+        console.log(key + ": " + person[key]);
+    }
+
+    continue
+    break
+*/
