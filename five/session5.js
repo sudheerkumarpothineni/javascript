f2cc29027ea63ba916720ffe2a5c62f7cac35d2b
sudheerkumarpothineni <sudheerchowdary543@gmail.com>
2025-10-29 22:43:40 +0530

    Javascript Day 5

---
+++ five/session5.js
@@ -0,0 +1,22 @@
+/*
+    Arrays
+    ------
+    Store diff data types into a single variable
+
+    Common array methods
+    --------------------
+    let fruits = ["apple", "banana", "guva"];
+
+    push() - add element at the end - fruits.push("orange");
+    pop() - removes the last element - fruits.pop();
+    unshift() - add the element at the first - fruits.unshift("mosambi");
+    shift() - removes the first element - fruits.shift();
+    indexOf() - get the index of element - fruits.indexOf("orange");
+    includes() - check wheather the value is there in the array or not - fruits.includes("orange");
+    join() - convert array to string - fruits.join(",");
+    concat() - merge arrays - fruits.concat(array2);
+    slice() - copy portion of array - fruits.slice(1,3);
+    splice() - add/remove elements - fruits.splice(1,2);
+    reverse() - reverse array - fruits.reverse();
+    sort() - sort elements - fruits.sort();
+*/
